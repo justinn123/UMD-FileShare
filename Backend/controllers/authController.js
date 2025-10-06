484c5a3a76b591ac412fb42558ef45e6ba8a3cae
Justin Nguyen <justinn123123123@gmail.com>
2025-10-06 15:50:24 -0400

    created backend api endpoints

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,28 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const signup = async (req, res) => {
+  try {
+    const {email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ email, password: hashedPassword });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    res.status(201).json({ token, user: { id: newUser._id, email: newUser.email } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
-
+import dotenv from "dotenv";
+import  authRoutes from "./routes/auth.js";
 
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT;
 
-console.log(PORT);
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 5000}`);
+  })
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
