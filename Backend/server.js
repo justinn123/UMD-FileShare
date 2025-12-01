4c5fdee3cfe61c293022f10873af90e89b432e99
Justin Nguyen <justinn123123123@gmail.com>
2025-12-01 16:42:09 -0500

    updated cors backend

--- Backend/server.js
+++ Backend/server.js
@@ -11,9 +11,17 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["https://umd-file-share.vercel.app/", "http://localhost:5173"],
+  origin: [
+    "https://umd-file-share.vercel.app",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+app.options("*", cors()); // handle preflight
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
