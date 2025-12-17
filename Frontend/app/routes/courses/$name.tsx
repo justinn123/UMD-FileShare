5a06b074732041dc192245397a3eabefb08be2f1
Justin Nguyen <justinn123123123@gmail.com>
2025-12-16 20:03:23 -0500

    removed unncessary toast

--- Frontend/app/routes/courses/$name.tsx
+++ Frontend/app/routes/courses/$name.tsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { optionalUser } from "~/utils/auth";
-import toast from "react-hot-toast";
 import Navbar from "~/components/header/navbar";
 import Footer from "~/components/footer";
 import CourseInfo from "~/components/courses/courseInfo";
@@ -35,22 +34,6 @@ export default function CoursePage() {
   useEffect(() => {
     optionalUser().then(({ user }) => {
       if (!user) {
-        toast.custom((t) => (
-          <div
-            className="flex items-center justify-between space-x-4 p-4 rounded-lg border
-               bg-gray-50 dark:bg-gray-800 border-gray-300 dark:border-gray-700"
-          >
-            <span className="text-sm font-medium text-gray-900 dark:text-gray-100">
-              Sign in to <b>upload files</b>
-            </span>
-            <button
-              className="ml-4 text-xs font-semibold text-gray-500 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-100"
-              onClick={() => toast.dismiss(t.id)}
-            >
-              Dismiss
-            </button>
-          </div>
-        ));
         setLoggedIn(false);
       } else {
         setLoggedIn(true);
